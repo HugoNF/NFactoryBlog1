4093b2a50886f87f5f1f342c3b81acc70bae356c
Frédéric Noël Conseil <contact@fredericnoel.com>
2018-01-12 16:00:38 +0100

    Modification formulaire (ajout de textarea)

---
+++ include/formArticle.php
@@ -0,0 +1,18 @@
+<form method="post" action="#">
+    <div>
+        <label for="title">Titre : </label>
+        <input type="text" name="title" />
+    </div>
+    <div>
+        <label for="chapo">Chapô :</label>
+        <input type="text" name="chapo" />
+    </div>
+    <div>
+        <label for="contenu">Contenu :</label>
+        <textarea id="article" name="article"></textarea>
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+</form>
