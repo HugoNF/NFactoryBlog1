2c6882892436c085b84e6ae743218f42da1d6a6d
Frédéric Noël Conseil <contact@fredericnoel.com>
2018-01-12 11:56:12 +0100

    Ajout condition login session

--- include/header.php
+++ include/header.php
@@ -2,6 +2,13 @@
 <ul>
     <li><a href="index.php?page=accueil">Accueil</a></li>
     <li><a href="index.php?page=inscription">Inscription</a></li>
-    <li><a href="index.php?page=login">Login</a></li>
+<?php
+    if ($_SESSION['login'] == 0 || !isset($_SESSION['login']))
+        echo ("<li><a href=\"index.php?page=login\">Login</a></li>");
+    else
+        echo ("<li><a href=\"index.php?page=logout\">Logout</a></li>");
+?>
+
+
 </ul>
 </header>
